f8658e1428cd503f40e483094a14fa12b15efc0d
benni-tec <me@benni-tec.de>
2023-10-28 17:23:22 +0200

    added TsxProviderBase and updatet TsxProvider to not break existing contracts // corrected and extended README.md

--- packages/tiled/lib/src/tiled_map.dart
+++ packages/tiled/lib/src/tiled_map.dart
@@ -286,7 +286,7 @@ class TiledMap {
     );
   }
 
-  factory TiledMap.parse(Parser parser, {List<TsxProvider>? tsxList}) {
+  factory TiledMap.parse(Parser parser, {List<TsxProviderBase>? tsxList}) {
     final backgroundColorHex = parser.getStringOrNull('backgroundcolor');
     final backgroundColor = parser.getColorOrNull('backgroundcolor');
     final compressionLevel = parser.getInt('compressionlevel', defaults: -1);
@@ -317,7 +317,7 @@ class TiledMap {
           return Tileset.parse(tilesetData);
         }
         final matchingTsx = tsxList.where(
-          (tsx) => tsx.filename == tilesetSource,
+          (tsx) => tsx.checkProvidable(tilesetSource),
         );
 
         return Tileset.parse(
